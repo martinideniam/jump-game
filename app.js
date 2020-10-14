fd2fd8597ec254770e6cfa529baf49cc772c7a0e
martinideniam <38355614+martinideniam@users.noreply.github.com>
2020-10-14 14:44:39 +0800

    added night mode

--- app.js
+++ app.js
@@ -2,39 +2,70 @@ class Game {
     constructor() {
         this.game = document.querySelector('.game');
         this.character = document.querySelector('.character')
+        this.html = document.querySelector('html');
+        this.h1 = document.querySelectorAll('h1');
         this.block = document.querySelector('.block')
         this.info = document.querySelector('.info')
         this.textdiv = document.querySelector('.text-game-div')
         this.textdivtext = document.querySelector('.text-game')
+        this.buttonLight = document.querySelector('.button-light')
+        this.buttonLightText = document.querySelector('.button-light-text');
         this.info.innerText = 'Tap screen to jump';
         this.button = document.querySelector('.button')
+        this.started = false;
         this.complexity = 1;
         this.counterInterval = null; 
+        this.night = true;
         this.start();
     }
 
     start() {
         this.button.addEventListener('click', () => {
-            this.character.classList.remove('hide');
-            this.block.classList.remove('hide');
-            this.textdiv.classList.add('hide');
-            this.game.classList.remove('flexstuff');
-            this.info.innerText = 'Tap screen to jump';
-            this.counter_number = 0;
-            this.checkIfFailed()
-            this.jump()
-            this.block.classList.add('animate_block');
-            //to make button not functioning when already started
-            if (!this.counterInterval) {
-                this.counterInterval = setInterval(() => {
-                    this.counter_number += 1
-                    this.info.innerText = `Your score: ${this.counter_number}`;
-                }, this.complexity*1000);
+            if (this.started == false) {
+                this.started = true; 
+                this.character.classList.remove('hide');
+                this.block.classList.remove('hide');
+                this.textdiv.classList.add('hide');
+                this.game.classList.remove('flexstuff');
+                this.info.innerText = 'Tap screen to jump';
+                this.counter_number = 0;
+                this.checkIfFailed()
+                this.jump()
+                this.block.classList.add('animate_block');
+                //to make button not functioning when already started
+                if (!this.counterInterval) {
+                    this.counterInterval = setInterval(() => {
+                        this.counter_number += 1
+                        this.info.innerText = `Your score: ${this.counter_number}`;
+                    }, this.complexity*1000);
+                }
+            }
+        })
+        this.buttonLight.addEventListener('click', ()=> {
+            if (this.night == true) {
+                this.night = false; 
+                this.buttonLightText.innerText = 'Dark mode';
+                this.html.style.background = 'white';
+                this.h1.forEach((element) => element.style.color = 'black');
+                this.buttonLight.style.border = '2px solid black';
+                this.button.style.border = '2px solid black';
+                this.character.style.background = 'black';
+                this.game.style.border = '2px solid black';
+            } else {
+                this.night = true; 
+                this.buttonLightText.innerText = 'Day mode';
+                this.html.style.background = 'black';
+                this.h1.forEach((element) => element.style.color = 'white');
+                this.buttonLight.style.border = '2px solid white';
+                this.button.style.border = '2px solid white';
+                this.character.style.background = 'white';
+                this.game.style.border = '2px solid white';
             }
         })
     }
 
     stop() {
+        this.started = false; 
         clearInterval(this.counterInterval);
         clearInterval(this.failedInterval);
         this.counterInterval = null;
@@ -50,14 +81,16 @@ class Game {
 
 
     jump() {
-        this.game.addEventListener('click', () => {
-            if (![...this.character.classList].includes('animate')) {
-                this.character.classList.add('animate');
-                this.character.addEventListener('animationend', function() {
-                    this.classList.remove('animate');
-                })
-            }
-        })
+        if (this.started == true) {
+            this.game.addEventListener('click', () => {
+                if (![...this.character.classList].includes('animate')) {
+                    this.character.classList.add('animate');
+                    this.character.addEventListener('animationend', function() {
+                        this.classList.remove('animate');
+                    })
+                }
+            })
+        }
     }
 
 
